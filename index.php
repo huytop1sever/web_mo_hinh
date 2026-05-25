997a638d2380e36d89251f075d7e2c9c665c2f65
PromptMiner <vmchuan25@gmail.com>
2026-05-25 12:22:42 +0700

    buil base

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- dùng include_once để tránh lỗi khi include nhiều lần
+dùng switch case để xử lý các trang khác nhau
+dùng isset để kiểm tra xem biến $_GET['page'] có tồn tại hay không
+dùng default case để xử lý khi không có trang nào được chọn
+hiển thị các trang cùng case tương ứng -->
+
+<?php
+include_once 'header.php';
+
+
+
+
+
+
+
+
