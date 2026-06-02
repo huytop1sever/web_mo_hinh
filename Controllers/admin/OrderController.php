c14b45b2476d6952d7aecc2443cbd48981220968
PromptMiner <vmchuan25@gmail.com>
2026-06-02 18:14:15 +0700

    chi tiet don hang

--- Controllers/admin/OrderController.php
+++ Controllers/admin/OrderController.php
@@ -15,4 +15,18 @@ class OrderController
 
         include_once __DIR__ . '/../../Views/admin/layouts/footer.php';
     }
+
+    public function detail()
+    {
+        $title = 'Chi tiết đơn hàng';
+        $pageTitle = 'Chi tiết đơn hàng';
+
+        include_once __DIR__ . '/../../Views/admin/layouts/header.php';
+        include_once __DIR__ . '/../../Views/admin/layouts/sidebar.php';
+        include_once __DIR__ . '/../../Views/admin/layouts/navbar.php';
+
+        include_once __DIR__ . '/../../Views/admin/orders/detail.php';
+
+        include_once __DIR__ . '/../../Views/admin/layouts/footer.php';
+    }
 }
