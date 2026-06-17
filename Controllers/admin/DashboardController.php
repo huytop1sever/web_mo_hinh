6a9f7f9b17df57c1950ed343676979f22e921cb8
Khanh-Duy99 <rongtrang110@gmail.com>
2026-06-17 13:16:22 +0700

    duongdan

--- Controllers/admin/DashboardController.php
+++ Controllers/admin/DashboardController.php
@@ -7,12 +7,12 @@ class DashboardController
         $title = 'Dashboard';
         $pageTitle = 'Dashboard';
 
-        include_once __DIR__ . '/../../Views/admin/layouts/header.php';
-        include_once __DIR__ . '/../../Views/admin/layouts/sidebar.php';
-        include_once __DIR__ . '/../../Views/admin/layouts/navbar.php';
+        include_once '../Views/admin/layouts/header.php';
+        include_once '../Views/admin/layouts/sidebar.php';
+        include_once '../Views/admin/layouts/navbar.php';
 
-        include_once __DIR__ . '/../../Views/admin/dashboard/index.php';
+        include_once '../Views/admin/dashboard/index.php';
 
-        include_once __DIR__ . '/../../Views/admin/layouts/footer.php';
+        include_once '../Views/admin/layouts/footer.php';
     }
 }
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,89 @@
 <?php
 
+session_start();
+
 require_once '../config/database.php';
-require_once '../routes/admin.php';
+
+// Models
+require_once '../Models/Product.php';
+require_once '../Models/Category.php';
+require_once '../Models/Post.php';
+require_once '../Models/User.php';
+require_once '../Models/Order.php';
+
+// Controllers
+require_once '../Controllers/admin/DashboardController.php';
+require_once '../Controllers/admin/CategoryController.php';
+require_once '../Controllers/admin/PostController.php';
+require_once '../Controllers/admin/ProductController.php';
+require_once '../Controllers/admin/UserController.php';
+require_once '../Controllers/admin/OrderController.php';
+
+$page = $_GET['page'] ?? 'dashboard';
+
+switch ($page) {
+
+    case 'dashboard':
+        (new DashboardController())->index();
+        break;
+
+    case 'products':
+        (new ProductController())->index();
+        break;
+
+    case 'product-add':
+        (new ProductController())->add();
+        break;
+
+    case 'product-edit':
+        (new ProductController())->edit($_GET['id'] ?? 0);
+        break;
+
+    case 'product-store':
+        (new ProductController())->store();
+        break;
+
+    case 'product-update':
+        (new ProductController())->update();
+        break;
+
+    case 'categories':
+        (new CategoryController())->index();
+        break;
+
+    case 'category-add':
+        (new CategoryController())->add();
+        break;
+
+    case 'category-edit':
+        (new CategoryController())->edit($_GET['id'] ?? 0);
+        break;
+
+    case 'category-store':
+        (new CategoryController())->store();
+        break;
+
+    case 'category-update':
+        (new CategoryController())->update();
+        break;
+
+    case 'category-delete':
+        (new CategoryController())->delete($_GET['id'] ?? 0);
+        break;
+
+    case 'posts':
+        (new PostController())->index();
+        break;
+
+    case 'orders':
+        (new OrderController())->index();
+        break;
+
+    case 'users':
+        (new UserController())->index();
+        break;
+
+    default:
+        (new DashboardController())->index();
+        break;
+}
